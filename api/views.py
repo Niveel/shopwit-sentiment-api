421500a1979eb03ed2ec544d06f7db610f8b6fbc
= <=>
2024-08-15 18:58:51 +0000

    add specific shop search functionaltiy

--- api/views.py
+++ api/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .sentiment import analyze_sentiment
 from .search import google_shopping_shop_search
-
+from .specific_shop_search import google_shopping_shop_search as sp_search
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-
 # Create your views here.
+
+api_key = 'd1bded552ea0e5d1a5c04c5794ad879e1ee3f5c3dff4626ac15de1b02c37e1c6'
 
 
 @api_view(["GET"])
@@ -18,7 +19,14 @@ def analyze(request):
 
 @api_view(["GET"])
 def general_search(request):
-    api_key = 'd1bded552ea0e5d1a5c04c5794ad879e1ee3f5c3dff4626ac15de1b02c37e1c6'
     query = request.GET.get("query")
     products = google_shopping_shop_search(api_key, query)
+    return Response(products)
+
+
+@api_view(["GET"])
+def search_shop(request):
+    query = request.GET.get("query")
+    shop = request.GET.get("shop")
+    products = sp_search(api_key, query, shop)
     return Response(products)
--- sentiment_showit/urls.py
+++ sentiment_showit/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import analyze, general_search
+from api.views import analyze, general_search, search_shop
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('analyze-text/', analyze, name="analyze_text"),
     path('general-search/', general_search, name="general_search"),
+    path('search-shop/', search_shop, name="search_shop"),
 ]
