febfe19eeba76b1624398fc32c1e5e751661ea39
= <=>
2024-08-15 07:29:11 +0000

    add general search functionality

--- sentiment_showit/urls.py
+++ sentiment_showit/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import  analyze
+from api.views import analyze, general_search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('analyze-text/', analyze, name="analyze_text")
+    path('analyze-text/', analyze, name="analyze_text"),
+    path('general-search/', general_search, name="general_search"),
 ]
